0830e8aa02402e2f59b5d52c7d746fda26f6f8f6
C3C Software <71038887+c3c-github@users.noreply.github.com>
2022-12-22 15:33:28 -0300

    Update customActivity.js

--- public/javascript/customActivity.js
+++ public/javascript/customActivity.js
@@ -102,6 +102,7 @@ function onGetEndpoints(endpoints) {
  * Save settings
  */
 function save() {
+    console.log('save');
     if($form.valid()) {
         payload['metaData'].isConfigured = true;
 
